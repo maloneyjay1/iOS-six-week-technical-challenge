ec577c8c06cbc84b91e029a649b9f29b89c42e9f
Jay Maloney <maloneyjay1@gmail.com>
2015-11-18 13:57:01 -0700

    Initial Commit

---
+++ week6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  week6
+//
+//  Created by Jay Maloney on 11/18/15.
+//  Copyright © 2015 Jay Maloney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
